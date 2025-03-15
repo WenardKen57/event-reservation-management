76ef5dc19b7e7ef785374eef9da9eb9dbcd0e7e7
Wenard Santos <wenardken57@gmail.com>
2025-03-15 13:41:15 +0800

    refactor: change laravel logos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,8 @@
                     @endphp
 
                     <a href="{{ $dashboardRoute }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <h1>Dashboard</h1>
+                        <!--<x-application-logo class="block h-9 w-auto fill-current text-gray-800" />-->
                     </a>
                 </div>
 
