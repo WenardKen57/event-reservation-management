7e5f79016e3843baa6930b64bfd190644b427583
Wenard Santos <wenardken57@gmail.com>
2025-03-15 14:46:51 +0800

    feat: add rentals table

---
+++ database/migrations/2025_03_15_064426_create_rentals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name')->nullable(false);
+            $table->enum('category', ['scaffold', 'table', 'chair', 'fabric', 'kitchen', 'backdrop', 'others'])
+            ->nullable(false);
+            $table->decimal('price_per_unit', total:10, places:2)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rentals');
+    }
+};
